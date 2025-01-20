11da2dbcd3a1858d5c0524564b4560936467a917
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2025-01-20 01:34:13 -0300

    array criado

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+
+function adicionarAmigo(){
+
+    let nomeAmigo = document.querySelector('input').value
+    if(nomeAmigo == ''){
+        alert("Insira um nome válido");
+    }else{
+        listaAmigos.push(nomeAmigo);
+        limparCampo();
+        console.log("nome cadastrado")
+        console.log(listaAmigos);
+    }   
+}
+
+function limparCampo(){
+    nomeAmigo = document.querySelector('input');
+    nomeAmigo.value = '';
+    
+}
