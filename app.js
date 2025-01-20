016e55fbef5ab51a7f834c98f26339090281cd1f
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2025-01-20 18:07:23 -0300

    lista e sorteio

--- app.js
+++ app.js
@@ -4,20 +4,42 @@ let listaAmigos = [];
 
 
 function adicionarAmigo(){
-
     let nomeAmigo = document.querySelector('input').value
     if(nomeAmigo == ''){
         alert("Insira um nome válido");
     }else{
         listaAmigos.push(nomeAmigo);
         limparCampo();
-        console.log("nome cadastrado")
-        console.log(listaAmigos);
+         // console.log("nome cadastrado")
+         // console.log(listaAmigos);
+        exibirNaLista();
     }   
+}
+
+function exibirNaLista() {  
+    let listaExibida = document.getElementById('listaAmigos')
+    listaExibida.innerHTML = '';
+    for (let i = 0; i < listaAmigos.length; i++) {
+        let itemLista = document.createElement('li');
+        itemLista.textContent = listaAmigos[i];
+        listaExibida.appendChild(itemLista);
+    }
+}
+
+function sortearAmigo(){
+    let resultadoExibido = document.getElementById('resultado');
+    if (listaAmigos.length < 1){
+        alert("Cadastre pelo menos 2 amigos")
+    }else{
+       let amigoSorteado = Math.floor(Math.random() * listaAmigos.length);
+       console.log(amigoSorteado);
+       resultadoExibido.innerHTML = listaAmigos[amigoSorteado];
+
+    }
+
 }
 
 function limparCampo(){
     nomeAmigo = document.querySelector('input');
     nomeAmigo.value = '';
-    
 }
